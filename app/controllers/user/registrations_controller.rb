6f625ff2dd090abc615a295833dfa3af8daf77b5
rosius <treyrosius@gmail.com>
2018-07-27 09:00:19 +0100

    added location in user model

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -4,8 +4,8 @@ class User::RegistrationsController < Devise::RegistrationsController
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name,:last_name])
-        devise_parameter_sanitizer.permit(:account_update, keys:[:first_name,:last_name])
+        devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name,:last_name,:location])
+        devise_parameter_sanitizer.permit(:account_update, keys:[:first_name,:last_name,:location])
 
     end
 
